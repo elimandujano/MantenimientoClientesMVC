60bff05b3c1cdc2add9e25ba14c6da63042948d3
Eli <Eli@VAIO_ELI>
2015-12-20 10:11:13 -0500

    creando las carpetas para el modelo MVC y el index.php

---
+++ Vista/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name = "description" content = "Aplicacion web con PHP para gestionar el mantenimiento
+              de los registros de la tabla clientes usando el patron MVC">
+        <meta name="keywords" content="PHP, CSS, HTML, Javascript, MVC">
+        <title>SISTEMA DE CONTROL DE VENTAS</title>
+    </head>
+    <body> <?php echo "Hola Mundo... Pagina en construcción" ?>
+        <header>
+            <?php
+                include '../Persistencia/conexion.php';
+            ?>            
+        </header>
+
+        <footer>
+            <?php
+                include 'pie.php';
+            ?>            
+        </footer>
+    </body>
+</html>
